42c6d07fc774172453a149c9aa1b432b96691944
Ralph Bolo <ralph.bolo@gmail.com>
2013-08-08 15:32:09 -0400

    Following other users is now working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,16 @@
 class User < ActiveRecord::Base
   attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
   has_secure_password
-
-	before_save :create_avatar_url
-  before_validation :prep_email
-
   has_many :ribbits
 
-  def create_avatar_url
-    self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
-	end
-
-  has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
-  has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
+  has_many :follower_relationships, class_name: 'Relationship', foreign_key: 'followed_id'
+  has_many :followed_relationships, class_name: 'Relationship', foreign_key: 'follower_id'
 
   has_many :followers, through: :follower_relationships
   has_many :followeds, through: :followed_relationships
 
+  before_validation :prep_email
+  before_save :create_avatar_url
 
   validates :name, presence: true
   validates :username, uniqueness: true, presence: true
@@ -25,14 +19,16 @@ class User < ActiveRecord::Base
   def following? user
     self.followeds.include? user
   end
- 
-def follow user
+  def follow user
     Relationship.create follower_id: self.id, followed_id: user.id
-end
-  
+  end
 
   private
   def prep_email
-  	self.email = self.email.strip.downcase if self.email
+    self.email = self.email.strip.downcase if self.email
+  end
+
+  def create_avatar_url
+    self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
   end
 end
