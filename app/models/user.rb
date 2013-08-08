d2eea764280612934a728e305dd72642bf7af3d7
Ralph Bolo <ralph.bolo@gmail.com>
2013-08-08 15:17:12 -0400

    created user relationships infrastructure

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,24 @@ class User < ActiveRecord::Base
     self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
 	end
 
+  has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
+  has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
+
+  has_many :followers, through: :follower_relationships
+  has_many :followeds, through: :followed_relationships
 
 
   validates :name, presence: true
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+
+  def following? user
+    self.followeds.include? user
+  end
+ 
+def follow user
+    Relationship.create follower_id: self.id, followed_id: user.id
+end
   
 
   private
